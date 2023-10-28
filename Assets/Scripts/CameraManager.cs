8ba13f7ffca670bb2b65bd995dc8d1ce71003020
DootDaMoop <jasonkhotsombath@gmail.com>
2023-10-27 23:25:34 -0400

    Added Camera Tracking

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private float cameraSpeed;
+    private GameObject target;
+
+    void Start() {
+        target = GameObject.FindWithTag("Player");
+    }
+
+    void Update() {
+        // Follows target (Player)
+        Vector3 pos = new Vector3(target.transform.position.x,target.transform.position.y,-10f);
+        transform.position = Vector3.Slerp(transform.position,pos,cameraSpeed*Time.deltaTime);
+    }
+}
